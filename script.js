6b7b9fb9af84233177b3fdadb8c32617c9cdb0df
saidzac2000 <147353811+saidzac2000@users.noreply.github.com>
2024-06-25 11:57:17 -0500

    V0.01
    the verry first version!!!!💯

---
+++ script.js
@@ -0,0 +1,62 @@
+let items = [];
+
+const itemsDiv = document.getElementById("items");
+
+const input = document.getElementById("itemsInput");
+
+const storageKey = "items";
+
+function loadeItems(){
+    const oldItems = localStorage.getItem(storageKey);
+    if(oldItems) items = JSON.parse(oldItems);
+    renderItems();
+}
+
+function renderItems(){
+    itemsDiv.innerHTML = null;
+
+    for (const [idx, item] of Object.entries(items)) {
+        const contaner = document.createElement("div");
+        contaner.style.marginBottom = "10px";
+
+        const text = document.createElement("p");
+        text.style.display = "inline";
+        text.style.marginRight = "10px"
+        text.textContent = item;
+
+        const button = document.createElement("button");
+        button.textContent = "Delete";
+        button.onclick = () => removeItems(idx);
+
+        contaner.appendChild(text);
+
+        contaner.appendChild(button);
+
+        itemsDiv.appendChild(contaner);
+    }
+}
+
+function saveItems(){
+    const stringItems = JSON.stringify(items);
+    localStorage.setItem(storageKey, stringItems);
+}
+
+function addItem(){
+    const value = input.value;
+    if(!value){
+        alert("You cannot add an empty item to youre Todo List");
+        return;
+    }
+    items.push(value);
+    renderItems();
+    input.value = "";
+    saveItems();
+}
+
+function removeItems(idx){
+    items.splice(idx, 1);
+    renderItems();
+    saveItems();
+}
+
+document.addEventListener("DOMContentLoaded", loadeItems)
